925b4630ee3c29c437d2b166bab26db41c8c947b
Дарья Фурзикова <dfurzikova@yandex.ru>
2018-10-14 21:00:27 +0300

    Добавила связь с версткой

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var app = express();
-var status = require('./status');
+const express = require('express');
+const app = express();
+const status = require('./status');
+const getData = require('./get-data');
 
 app.get('/status', function (req, res) {
     res.send(status());
+});
+
+app.get('/api/events', function (req, res) {
+    const result = getData(req, res);
+
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+
+    if (result.status) {
+        res.status(result.status);
+    }
+
+    res.send(result.response);
+});
+
+app.get('/*', function (req, res) {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(8000, function () {
